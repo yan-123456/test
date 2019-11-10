ce955ec505634c99a5a82967e6da1805a5ceb5b8
Administrator <12>
2019-11-10 17:05:35 +0800

    dierci

---
+++ src/test/java/com/jk/GittowApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittowApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(121);
+    }
+
+}
